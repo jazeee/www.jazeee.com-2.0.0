89f5218893fe3b15d70de7f1ea741f520346dd3d
Jaz Singh <jsingh@practichem.com>
2019-07-14 15:19:26 -0700

    Reorganize and clean up

--- src/pages/skills/index.js
+++ src/pages/skills/index.js
@@ -1,7 +1,31 @@
 import React from "react";
 import { Layout } from "../../components/layout";
 import { SEO } from "../../components/seo";
-import { Skills } from "./skills";
+import styles from "./skills.module.css";
+import { skills } from "../../data/utils";
+
+const webAppSkillNames = skills
+  .getDomainSkillNames("Software Engineering")
+  .filter((skill, index) => index < 12);
+
+export const Skills = () => (
+  <div className={styles.aboutJaz}>
+    <div className={styles.container}>
+      <div className={styles.skillsHeaderOverlay}>
+        <h1> Full Stack Web Developer</h1>
+        <h4>Primary Developer Skills (Sorted by skill level)</h4>
+        <ul>
+          {webAppSkillNames.map(skillName => (
+            <li key={skillName}>{skillName}</li>
+          ))}
+        </ul>
+        <a href="http://goo.gl/qWsPm" target="_blank" rel="noopener noreferrer">
+          Resume
+        </a>
+      </div>
+    </div>
+  </div>
+);
 
 const SkillsPage = () => (
   <Layout>
--- src/pages/software-projects/index.js
+++ src/pages/software-projects/index.js
@@ -1,7 +1,28 @@
 import React from "react";
+import Container from "@material-ui/core/Container";
+import Button from "@material-ui/core/Button";
+import { SOFTWARE_PROJECTS } from "../../data/software-projects-data";
+import styles from "./software-projects.module.css";
+import { GithubProject } from "../../components/projects/github-project";
+import { Link } from "../../components/link";
 import { Layout } from "../../components/layout";
 import { SEO } from "../../components/seo";
-import { SoftwareProjects } from "./software-projects";
+
+export const SoftwareProjects = () => (
+  <Container className={styles.projects}>
+    <h1 className={styles.header}>Software Projects</h1>
+    {SOFTWARE_PROJECTS.map(project => {
+      return <GithubProject key={project.name} project={project} />;
+    })}
+    <Container className={styles.actions}>
+      <Button variant="contained">
+        <Link to="software-projects/all-github-projects">
+          All Github Projects
+        </Link>
+      </Button>
+    </Container>
+  </Container>
+);
 
 const SoftwareProjectsPage = () => (
   <Layout>
